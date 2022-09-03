4c3c0871545fb4ba034f5564062927de5f5b5b99
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-09-03 09:19:22 +0600

    server

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var http = require("http");
 const _ = require("underscore");
 const arr = [
   { name: "afridi", roll: "1" },
@@ -5,18 +6,20 @@ const arr = [
   { name: "afridi2", roll: "1" },
 ];
 const add = require("./other");
-const res = add(4, 5);
-console.log(res);
-var http = require("http");
+// const resu = add(4, 5);
+// console.log(resu);
 
-var server = http.createServer(function (req, res) {
+console.log(http);
+
+var server = http.createServer((req, res) => {
   //write code here
   console.log(`Server is running`);
+  res.end("local host");
   //   console.log(req.body);
 });
 const name = _.pluck(arr, "name");
 console.log(name);
 
-server.listen(5000);
+server.listen(4000);
 
 // server.listen(5000);
